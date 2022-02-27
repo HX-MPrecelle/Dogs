cfd44bba4f1e87da3431b1b3285a5bd2ccb15691
MPrecelle <tinchoprecelle@gmail.com>
2022-02-27 17:03:48 -0300

    Create db.js and basic models

---
+++ api/src/models/Dog.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    height: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+    breed_group: {
+        type: DataTypes.STRING,
+    },
+    life_span: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    temperaments: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+    },
+  });
+};
