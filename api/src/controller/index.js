6d5824bb167a5ebeaff19aea4cd4d7cf007db33b
MPrecelle <tinchoprecelle@gmail.com>
2022-02-27 22:25:43 -0300

    Push dogs and temperaments in db. Fix connections

--- api/src/controller/index.js
+++ api/src/controller/index.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const e = require("express");
 const { Dog, Temperament } = require("../db");
 const { API_KEY } = process.env;
 
@@ -6,7 +7,8 @@ const { API_KEY } = process.env;
 const getApiDogs = async () => {
   const url = `https://api.thedogapi.com/v1/breeds/?api_key=${API_KEY}`;
   const allData = await axios.get(url);
-  const dogs = allData.data?.map((e) => {
+  const allDogs = allData.data;
+  const dogs = await allDogs?.map((e) => {
     return {
       name: e.name,
       weight: e.weight?.metric,
@@ -14,11 +16,10 @@ const getApiDogs = async () => {
       bred_for: e.bred_for ? e.bred_for : "Unknown",
       breed_group: e.breed_group ? e.breed_group : "Unknown",
       life_span: e.life_span,
-      temperaments: e.temperament ? e.temperament.split(",") : "Unknown",
+      temperament: e.temperament ? e.temperament.split(",") : ["Unknown"],
       image: e.image?.url,
     };
   });
-  // console.log(dogs);
   dogs?.forEach((d) => {
     Dog.findOrCreate({
       where: {
@@ -30,10 +31,37 @@ const getApiDogs = async () => {
         bred_for: d.bred_for,
         breed_group: d.breed_group,
         life_span: d.life_span,
-        temperaments: d.temperaments,
+        temperament: d.temperament.map((e) => e),
         image: d.image,
       },
     });
   });
   return dogs;
+};
+
+const getApiTemperaments = async () => {
+  const url = `https://api.thedogapi.com/v1/breeds/?api_key=${API_KEY}`;
+  const allData = await axios.get(url);
+  const allTemperaments = allData.data?.map((e) => e.temperament?.split(","));
+  const copyTemperaments = [];
+  // console.log(allTemperaments);
+  for (const temperament of allTemperaments) {
+    temperament?.forEach((t) => copyTemperaments.push(t.trim()));
+    // console.log(temperament);
+  }
+  const temperaments = [...new Set(copyTemperaments)];
+//   console.log(temperaments);
+  temperaments?.forEach(t => {
+      Temperament.findOrCreate({
+          where: {
+              name: t,
+          }
+      });
+  });
+  return temperaments;
+};
+
+module.exports = {
+  getApiDogs,
+  getApiTemperaments,
 };
